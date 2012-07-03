75c78a0893e00d7ec3d42bbacc5354a109b23a9c
Alon Horev <alonho@gmail.com>
2012-07-03 13:52:50 +0300

    add metaclasses

--- make.sh
+++ make.sh
@@ -1,1 +1,4 @@
-landslide -r -c -ltable doc && open presentation.html
+landslide -d decorators.html -r -c -ltable doc/decorators.md
+landslide -d args.html -r -c -ltable doc/args.md
+landslide -d staticclass.html -r -c -ltable doc/staticclass.md
+
--- meta.py
+++ meta.py
@@ -1,6 +1,20 @@
-class Person(models.Model):
-    name = models.CharField(max_length=30)
-    age = models.IntegerField()
+import inspect
 
-guy = Person(name='bob', age='35')
-print guy.age
+def interface(interface_class):
+    class Interface(type):
+        def __new__(cls, name, bases, dct):
+            for interface_attr, interface_value in interface_class.__dict__.iteritems():
+                if not inspect.isfunction(interface_value):
+                    continue
+                if interface_attr not in dct:
+                    raise NotImplementedError('{} is not implemented for {}'.format(interface_attr, name))
+                check_signatures(interface_value, dct[interface_attr])
+    return Interface
+
+def check_signatures(orig, new):
+    if not inspect.isfunction(new):
+        raise NotImplementedError('{} should be a function but is a {}'.format(new, type(new)))
+    orig_args = inspect.getargspec(orig).args
+    new_args = inspect.getargspec(new).args
+    if orig_args != new_args:
+        raise NotImplementedError('argument diff in {}: {} vs. {}'.format(new, orig_args, new_args))
